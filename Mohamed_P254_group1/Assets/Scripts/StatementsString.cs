7267447b5b080085bc3c36033798864fca5e27da
MohamedBasyouny <basyouny58@gmail.com>
2021-12-20 04:56:45 +0200

    new script created

---
+++ Mohamed_P254_group1/Assets/Scripts/StatementsString.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatementsString : MonoBehaviour
+{
+    public string name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (name == "Mohamed")
+        {
+            print("You are Mohamed");
+        }
+        else
+        {
+            print("You are not Mohamed");
+        }
+    }
+}
