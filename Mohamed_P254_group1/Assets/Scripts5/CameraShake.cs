222a35a955fbfb2d0c5dc1379622879f8af9aae3
MohamedBasyouny <basyouny58@gmail.com>
2021-12-26 04:48:01 +0200

    Practice5

---
+++ Mohamed_P254_group1/Assets/Scripts5/CameraShake.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    Transform CamTransform; // camera transform params
+    public float shakeTime; // how long we want to shake our camera 
+    public float shakeRange; // how far we want to move our camera 
+    Vector3 originalpos; // restore original cam pos
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CamTransform = Camera.main.transform;
+        originalpos = CamTransform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(ShakeCamera()); // StartCoroutine("ShakeCamera");
+        }
+    }
+
+    IEnumerator ShakeCamera()
+    {
+        float elapsedTime = 0;
+        while(elapsedTime < shakeTime)
+        {
+           CamTransform.position = Random.insideUnitSphere* shakeRange;
+            elapsedTime += Time.deltaTime;
+            yield return null;
+
+
+        }
+        CamTransform.position = originalpos;
+
+    }
+}
